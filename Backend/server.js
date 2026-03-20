2738fed0c6eb432dd6f28da1f3ec497b7857d359
Tanvi Ganotra <151958302+TanviGanotra30@users.noreply.github.com>
2026-03-20 18:33:08 +0530

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,9 +8,11 @@ const aiRoutes = require("./routes/aiRoutes")
 require("dotenv").config()
 
 const app = express()
+
 app.use(cors({
   origin: "*"
-}));
+}))
+
 app.use(express.json())
 
 app.use("/api/auth", authRoutes)
@@ -18,10 +20,10 @@ app.use("/api/session", sessionRoutes)
 app.use("/api/ai", aiRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB connected"))
-.catch(err=>console.log(err))
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err))
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.send("StudyFlow API running")
 })
 
